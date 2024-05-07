12d0ee11e77efa04239e711f80dad80dea43c5c1
Jx Yi <79647385+janshirley@users.noreply.github.com>
2024-05-07 22:15:15 +0100

    Add files via upload

--- packages/questions/src/data/zh-cn/choices.ts
+++ packages/questions/src/data/zh-cn/choices.ts
@@ -1,54 +1,54 @@
 const choices = {
   plus: [
     {
-      text: '错误',
+      text: '非常不准确',
       score: 1,
       color: 1
     },
     {
-      text: '失准',
+      text: '比较不准确',
       score: 2,
       color: 2
     },
     {
-      text: '普通',
+      text: '不确定',
       score: 3,
       color: 3
     },
     {
-      text: '准确',
+      text: '比较准确',
       score: 4,
       color: 4
     },
     {
-      text: '精确',
+      text: '非常准确',
       score: 5,
       color: 5
     }
   ],
   minus: [
     {
-      text: '错误',
+      text: '非常不准确',
       score: 5,
       color: 1
     },
     {
-      text: '失准',
+      text: '比较不准确',
       score: 4,
       color: 2
     },
     {
-      text: '普通',
+      text: '不确定',
       score: 3,
       color: 3
     },
     {
-      text: '准确',
+      text: '比较准确',
       score: 2,
       color: 4
     },
     {
-      text: '精确',
+      text: '非常准确',
       score: 1,
       color: 5
     }
--- packages/questions/src/data/zh-cn/questions.ts
+++ packages/questions/src/data/zh-cn/questions.ts
@@ -1,42 +1,42 @@
 const questions = [
   {
     id: '43c98ce8-a07a-4dc2-80f6-c1b2a2485f06',
-    text: '杞人忧天',
+    text: '为很多事情感到担心',
     keyed: 'plus',
     domain: 'N',
     facet: 1
   },
   {
     id: 'd50a597f-632b-4f7b-89e6-6d85b50fd1c9',
-    text: '平易近人',
+    text: '很容易交到朋友',
     keyed: 'plus',
     domain: 'E',
     facet: 1
   },
   {
     id: '888dd864-7449-4e96-8d5c-7a439603ea91',
-    text: '天马行空',
+    text: '有丰富的想象力',
     keyed: 'plus',
     domain: 'O',
     facet: 1
   },
   {
     id: 'ce2fbbf8-7a97-4199-bda5-117e4ecdf3b6',
-    text: '推心置腹',
+    text: '信任其他人',
     keyed: 'plus',
     domain: 'A',
     facet: 1
   },
   {
     id: 'c7f53c3c-2e77-432f-bb71-7470b67d3aa9',
-    text: '不辱使命',
+    text: '能成功完成任务',
     keyed: 'plus',
     domain: 'C',
     facet: 1
   },
   {
     id: '48ad12ce-470e-4339-90ac-ea8c43a0103e',
-    text: '易怒',
+    text: '很容易生气',
     keyed: 'plus',
     domain: 'N',
     facet: 2
@@ -50,84 +50,84 @@ const questions = [
   },
   {
     id: '58d571e5-d725-4cf8-a438-32c16ee28eb6',
-    text: '相信艺术的重要性',
+    text: '认为艺术是很重要的',
     keyed: 'plus',
     domain: 'O',
     facet: 2
   },
   {
     id: '0cf79e27-e702-45c2-9471-04ac96b58e0e',
-    text: '借刀杀人',
+    text: '会利用他人来达成自己的目的',
     keyed: 'minus',
     domain: 'A',
     facet: 2
   },
   {
     id: 'cda1ca17-b599-4561-a6cd-ff9d36062d27',
-    text: '洁身自好',
+    text: '喜欢整洁',
     keyed: 'plus',
     domain: 'C',
     facet: 2
   },
   {
     id: '5e8550d7-b8ef-4905-950a-f81d735d39e2',
-    text: '易伤感',
+    text: '时常感到忧郁',
     keyed: 'plus',
     domain: 'N',
     facet: 3
   },
   {
     id: '8af754f2-68e9-48f3-8c5d-2e6633d4472c',
-    text: '负责',
+    text: '承担责任',
     keyed: 'plus',
     domain: 'E',
     facet: 3
   },
   {
     id: '0727def6-3d18-4221-bf38-86b58f9f3eed',
-    text: '自我陶醉',
+    text: '能强烈感受到自己的情绪',
     keyed: 'plus',
     domain: 'O',
     facet: 3
   },
   {
     id: 'ccf3a5c8-fb50-4bd4-8e7a-22af3d657279',
-    text: '乐于助人',
+    text: '乐于帮助他人',
     keyed: 'plus',
     domain: 'A',
     facet: 3
   },
   {
     id: '73d84e5d-cbf5-47f0-b8cb-4d2159a52e32',
-    text: '遵守诺言',
+    text: '信守承诺',
     keyed: 'plus',
     domain: 'C',
     facet: 3
   },
   {
     id: 'b2d9ef74-73f5-4ea8-b00c-7aaca15937df',
-    text: '善察言观色',
+    text: '感觉自己很难接近别人',
     keyed: 'plus',
     domain: 'N',
     facet: 4
   },
   {
     id: '48a761ef-438e-409b-ae59-ea2ce8f84414',
-    text: '日理万机',
+    text: '总是很忙碌',
     keyed: 'plus',
     domain: 'E',
     facet: 4
   },
   {
     id: 'cae55842-8957-4e3b-83b3-ceff98fb9dcf',
-    text: '喜欢多样化，不喜欢千篇一律',
+    text: '相比墨守成规，我更喜欢有变化',
     keyed: 'plus',
     domain: 'O',
     facet: 4
   },
   {
     id: 'e2028ad3-b128-4f76-be57-398bfe2aff22',
-    text: '爱干架',
+    text: '喜欢和别人比试',
     keyed: 'minus',
     domain: 'A',
     facet: 4
@@ -141,112 +141,112 @@ const questions = [
   },
   {
     id: '481efd08-c810-43b1-a952-f8ac9052f96b',
-    text: '狂欢作乐',
+    text: '大吃大喝寻欢作乐',
     keyed: 'plus',
     domain: 'N',
     facet: 5
   },
   {
     id: '987efee2-899f-4a65-b9b5-1589ef0460d7',
-    text: '喜爱刺激',
+    text: '喜欢追求刺激',
     keyed: 'plus',
     domain: 'E',
     facet: 5
   },
   {
     id: 'e1e804c7-4a1d-498f-8610-f95147af9d1d',
-    text: '喜爱挑战',
+    text: '喜欢阅读有挑战性的材料',
     keyed: 'plus',
     domain: 'O',
     facet: 5
   },
   {
     id: '71029381-3908-4c68-91e1-e41fb45542a2',
-    text: '自信',
+    text: '相信自己比别人更好',
     keyed: 'minus',
     domain: 'A',
     facet: 5
   },
   {
     id: 'f6076eea-56ae-4b46-97f1-5f94a7676c96',
-    text: '时刻有准备',
+    text: '我总是有所准备',
     keyed: 'plus',
     domain: 'C',
     facet: 5
   },
   {
     id: '2f519935-92e8-48ad-9746-4a0f8b38466a',
-    text: '易惊慌失措',
+    text: '容易慌张',
     keyed: 'plus',
     domain: 'N',
     facet: 6
   },
   {
     id: '899c3f66-51d0-46ea-963a-6fc36d3b3cb9',
-    text: '流露出欢乐',
+    text: '我的快乐会感染周围的人',
     keyed: 'plus',
     domain: 'E',
     facet: 6
   },
   {
     id: '79186f48-e7fa-4df4-b74b-b0627ee244e1',
-    text: '倾向于投票给自由派的政治候选人',
+    text: '认为自己在政治方面非常自由开放',
     keyed: 'plus',
     domain: 'O',
     facet: 6
   },
   {
     id: 'fd50e1ca-d9e0-4037-a7a1-a191d4db2d96',
-    text: '同情无家可归之人',
+    text: '同情无家可归之人(同情流浪汉 xu,2014)',
     keyed: 'plus',
     domain: 'A',
     facet: 6
   },
   {
     id: 'bd9eec0a-b68b-472c-8803-7db29c308cdb',
-    text: '不假思索地做事情',
+    text: '会不经过思考就开始行动',
     keyed: 'minus',
     domain: 'C',
     facet: 6
   },
   {
     id: '7f92ab2c-265c-4b84-8c74-09f9bb9d41a7',
-    text: '作最坏的打算',
+    text: '担心会发生最糟糕的情况',
     keyed: 'plus',
     domain: 'N',
     facet: 1
   },
   {
     id: 'af55f014-788c-4b6e-92c4-b2b59dc8a28d',
-    text: '感觉周围的人很好',
+    text: '和其他人待在一起时，我觉得很自在',
     keyed: 'plus',
     domain: 'E',
     facet: 1
   },
   {
     id: '08ff6dca-02a5-4aeb-aaa4-2ecf2526f143',
-    text: '享受疯狂的幻想飞行',
+    text: '喜欢天马行空地幻想',
     keyed: 'plus',
     domain: 'O',
     facet: 1
   },
   {
     id: '6f66cdc0-9044-457b-b40d-501ecae15ee7',
-    text: '相信别人有好的意图',
+    text: '相信他人的出发点是好的',
     keyed: 'plus',
     domain: 'A',
     facet: 1
   },
   {
     id: 'f110fc66-2e9e-413c-920b-19f05e63d7ac',
-    text: '擅长我的工作',
+    text: '在自己所从事的领域出类拔萃',
     keyed: 'plus',
     domain: 'C',
     facet: 1
   },
   {
     id: '7dab2a37-8635-4fc7-86b7-0abf13c183c9',
-    text: '很容易生气',
+    text: '容易被激怒',
     keyed: 'plus',
     domain: 'N',
     facet: 2
@@ -274,7 +274,7 @@ const questions = [
   },
   {
     id: 'adf33f9f-45bd-43e3-af25-4c491176d97f',
-    text: '健忘',
+    text: '经常忘记把东西放回原处',
     keyed: 'minus',
     domain: 'C',
     facet: 2
@@ -288,14 +288,14 @@ const questions = [
   },
   {
     id: '0b38e3d3-c15c-454c-b034-f4eb7ae1580a',
-    text: '试着去领导别人',
+    text: '尝试去领导他人',
     keyed: 'plus',
     domain: 'E',
     facet: 3
   },
   {
     id: '5631b856-ff34-4f76-a0cd-edc7104c3bfa',
-    text: '感受别人的情绪',
+    text: '能感受到他人的情绪',
     keyed: 'plus',
     domain: 'O',
     facet: 3
@@ -309,7 +309,7 @@ const questions = [
   },
   {
     id: 'c55e3958-00c4-4fc3-9118-47d8f31bfde1',
-    text: '诚实',
+    text: '我会实话实说',
     keyed: 'plus',
     domain: 'C',
     facet: 3
@@ -330,28 +330,28 @@ const questions = [
   },
   {
     id: '33b81fd0-7e32-4cd8-a13a-d5f5f754f998',
-    text: '喜欢坚持我知道的事情',
+    text: '喜欢做自己熟悉的事情',
     keyed: 'minus',
     domain: 'O',
     facet: 4
   },
   {
     id: 'd9a9a180-29c9-4ec5-8621-2256d411def7',
-    text: '对人们大喊大叫',
+    text: '会冲人大喊大叫',
     keyed: 'minus',
     domain: 'A',
     facet: 4
   },
   {
     id: 'f12c3d9d-1d12-4aa6-ad2e-009cd0651cbb',
-    text: '期望做的更多',
+    text: '会超出预期地完成任务或工作',
     keyed: 'plus',
     domain: 'C',
     facet: 4
   },
   {
     id: '9891b7ba-a494-4307-aafe-301d8db506c6',
-    text: '很少放纵',
+    text: '很少过度放纵',
     keyed: 'minus',
     domain: 'N',
     facet: 5
@@ -365,7 +365,7 @@ const questions = [
   },
   {
     id: '95a3f20c-f933-4d19-a2c1-a7dbdf63c562',
-    text: '避免进行哲学讨论',
+    text: '避免参与关于哲学的讨论',
     keyed: 'minus',
     domain: 'O',
     facet: 5
@@ -379,56 +379,56 @@ const questions = [
   },
   {
     id: '9d3cb5c7-955c-43a4-b6c7-b07ed01dcbd9',
-    text: '执行我的计划',
+    text: '能执行自己制定的计划',
     keyed: 'plus',
     domain: 'C',
     facet: 5
   },
   {
     id: '13c58810-3864-42ba-aa87-d4166f858756',
-    text: '被各种事情淹没',
+    text: '感到被各种事情压得喘不过气来',
     keyed: 'plus',
     domain: 'N',
     facet: 6
   },
   {
     id: '961376e0-16a1-4c14-b059-789e63d11b63',
-    text: '玩得很开心',
+    text: '我的生活很快乐',
     keyed: 'plus',
     domain: 'E',
     facet: 6
   },
   {
     id: 'f08e1b27-3673-4898-9cae-896482d0d9f9',
-    text: '相信没有绝对的对与错',
+    text: '相信没有绝对的对错之分',
     keyed: 'plus',
     domain: 'O',
     facet: 6
   },
   {
     id: 'c2038c12-7a37-47a8-9983-831bd6692aab',
-    text: '同情那些比我糟糕的人',
+    text: '我同情那些处境不如自己的人',
     keyed: 'plus',
     domain: 'A',
     facet: 6
   },
   {
     id: '956f3e17-ff17-4af5-a52f-9222b8968106',
-    text: '鲁莽行事',
+    text: '会做出草率的决定',
     keyed: 'minus',
     domain: 'C',
     facet: 6
   },
   {
     id: '4d81238b-5407-47d4-88e5-dc0e38aa14f5',
-    text: '害怕很多事情',
+    text: '对很多事情感到害怕',
     keyed: 'plus',
     domain: 'N',
     facet: 1
   },
   {
     id: '9f9166f0-fa94-4c14-a91d-3eecd8395794',
-    text: '避免与他人接触',
+    text: '避免和他人接触',
     keyed: 'minus',
     domain: 'E',
     facet: 1
@@ -442,28 +442,28 @@ const questions = [
   },
   {
     id: 'c63e6121-c3ed-40cc-abc2-c1e6ea1e0858',
-    text: '相信别人所说的',
+    text: '相信他人说的话',
     keyed: 'plus',
     domain: 'A',
     facet: 1
   },
   {
     id: '02ee1930-36a7-4caa-b10c-c93efb682a44',
-    text: '顺利处理任务',
+    text: '能轻松完成任务',
     keyed: 'plus',
     domain: 'C',
     facet: 1
   },
   {
     id: 'da8e6ed1-2296-4c58-8fdb-66f2f591989b',
-    text: '开始发脾气',
+    text: '发脾气',
     keyed: 'plus',
     domain: 'N',
     facet: 2
   },
   {
     id: '03c10b30-b88f-4c63-8acc-71251ca24615',
-    text: '喜欢独自一人',
+    text: '更喜欢一个人独处',
     keyed: 'minus',
     domain: 'E',
     facet: 2
@@ -484,7 +484,7 @@ const questions = [
   },
   {
     id: 'f4891687-0ff0-47af-a4f6-d1202c8f6676',
-    text: '我的房间一团糟',
+    text: '我的房间很乱',
     keyed: 'minus',
     domain: 'C',
     facet: 2
@@ -498,49 +498,49 @@ const questions = [
   },
   {
     id: '2452f034-8273-4f71-9122-a40f5ead31ba',
-    text: '掌控一切',
+    text: '掌控局面',
     keyed: 'plus',
     domain: 'E',
     facet: 3
   },
   {
     id: '2a300001-6e05-4c79-b8b5-2ccae4c3d463',
-    text: '很少注意到我的情绪反应',
+    text: '很少察觉到自己的情绪反应',
     keyed: 'minus',
     domain: 'O',
     facet: 3
   },
   {
     id: 'cd54bd76-ca9c-4030-b325-bb8d896bcb3f',
-    text: '对别人漠不关心',
+    text: '对他人的感受漠不关心',
     keyed: 'minus',
     domain: 'A',
     facet: 3
   },
   {
     id: '4e6e3a34-176f-4e6e-8730-1341611f972b',
-    text: '打破规则',
+    text: '破坏规则',
     keyed: 'minus',
     domain: 'C',
     facet: 3
   },
   {
     id: '20062533-a33d-4c1e-9cd9-bff868015b3f',
-    text: '只有和朋友在一起才觉得舒服',
+    text: '只有和朋友在一起的时候我才会感到自在',
     keyed: 'plus',
     domain: 'N',
     facet: 4
   },
   {
     id: 'b2a077d5-1fe0-4b06-ab63-35455e001e54',
-    text: '在业余时间做很多事',
+    text: '我的闲暇时间非常充实',
     keyed: 'plus',
     domain: 'E',
     facet: 4
   },
   {
     id: '0d2e65ab-95d9-482f-beb4-3239a3a4944a',
-    text: '比喜欢改变',
+    text: '不喜欢改变',
     keyed: 'minus',
     domain: 'O',
     facet: 4
@@ -554,49 +554,49 @@ const questions = [
   },
   {
     id: 'a9c97d6b-6721-4150-8d84-64ef3082f164',
-    text: '完成足够的工作',
+    text: '在工作或学习上，我不会多做，过得去就行',
     keyed: 'minus',
     domain: 'C',
     facet: 4
   },
   {
     id: '9f2e7f90-0ca5-4ed0-9fe5-e060238a9b5e',
-    text: '轻松抵制诱惑',
+    text: '能轻松抵御诱惑',
     keyed: 'minus',
     domain: 'N',
     facet: 5
   },
   {
     id: '7dd6cf2d-5c14-48c2-8ae5-633a7a596c71',
-    text: '做事不计后果',
+    text: '喜欢不计后果地行事',
     keyed: 'plus',
     domain: 'E',
     facet: 5
   },
   {
     id: 'fecc35f7-681e-4889-a404-4a973a3dfef0',
-    text: '难以理解抽象的概念',
+    text: '理解抽象概念有些困难',
     keyed: 'minus',
     domain: 'O',
     facet: 5
   },
   {
     id: '1d686958-6fe7-432f-85e6-186b99e4e232',
-    text: '对自己有很高的评价',
+    text: '对自己的评价很高',
     keyed: 'minus',
     domain: 'A',
     facet: 5
   },
   {
     id: 'c7db0ed8-df7d-49bf-942f-59e46ef743c4',
-    text: '浪费我的时间',
+    text: '浪费时间',
     keyed: 'minus',
     domain: 'C',
     facet: 5
   },
   {
     id: 'b7e0e393-9b21-4e0d-adf3-8f28fb5b9d87',
-    text: '觉得我无法处理事情',
+    text: '感觉自己处理事情力不从心',
     keyed: 'plus',
     domain: 'N',
     facet: 6
@@ -610,42 +610,42 @@ const questions = [
   },
   {
     id: '96ba77b2-1a44-4dfd-95f9-ae4d1f714460',
-    text: '倾向于投票给保守派的政治候选人',
+    text: '我在政治上比较保守',
     keyed: 'minus',
     domain: 'O',
     facet: 6
   },
   {
     id: '77f54ab4-0fba-4efb-8700-066c7490eb87',
-    text: '对他人的问题不感兴趣',
+    text: '对别人遇到的麻烦事不感兴趣',
     keyed: 'minus',
     domain: 'A',
     facet: 6
   },
   {
     id: 'a354cf7c-8d11-46ac-acc5-da90d2048637',
-    text: '做事急躁',
+    text: '仓促行事',
     keyed: 'minus',
     domain: 'C',
     facet: 6
   },
   {
     id: '43b03992-3f32-4ed1-a6f8-5d6d3e7ed246',
-    text: '易感受到压力',
+    text: '容易感受到压力',
     keyed: 'plus',
     domain: 'N',
     facet: 1
   },
   {
     id: '41702602-08e4-4e2b-9a19-291d9efc581a',
-    text: '与人保持距离',
+    text: '和他人保持距离',
     keyed: 'minus',
     domain: 'E',
     facet: 1
   },
   {
     id: '935a7413-abac-4f54-9169-d1fbd39da752',
-    text: '喜欢沉思',
+    text: '喜欢陷入沉思',
     keyed: 'plus',
     domain: 'O',
     facet: 1
@@ -659,28 +659,28 @@ const questions = [
   },
   {
     id: '5727c93f-317b-4af1-a686-77fc9fbc5033',
-    text: '知道如何把事情做好',
+    text: '知道如何完成任务',
     keyed: 'plus',
     domain: 'C',
     facet: 1
   },
   {
     id: 'd32bd062-4eb2-401b-99b2-e7afea39ca9b',
-    text: '不轻易生气',
+    text: '不会轻易被惹恼',
     keyed: 'minus',
     domain: 'N',
     facet: 2
   },
   {
     id: '9a47184f-6046-4e68-a61b-3d9b357b86ea',
-    text: '不随波逐流',
+    text: '避开人多的地方',
     keyed: 'minus',
     domain: 'E',
     facet: 2
   },
   {
     id: '87c5b27e-59a8-4c48-8ba8-f5413d735693',
-    text: '不喜欢去艺术博物馆',
+    text: '不喜欢去美术馆',
     keyed: 'minus',
     domain: 'O',
     facet: 2
@@ -694,63 +694,63 @@ const questions = [
   },
   {
     id: '50418d86-712c-45d9-adc4-ea0231c93cf5',
-    text: '忘记我的随身物品',
+    text: '我的东西放得到处都是',
     keyed: 'minus',
     domain: 'C',
     facet: 2
   },
   {
     id: 'f40e421f-6c24-4be2-bd9f-28d33358d8c6',
-    text: '感觉自己挺好',
+    text: '对自己感到满意',
     keyed: 'minus',
     domain: 'N',
     facet: 3
   },
   {
     id: '8791f37b-686f-47c3-9db7-74c009951321',
-    text: '等待他人带路',
+    text: '等着别人来带头',
     keyed: 'minus',
     domain: 'E',
     facet: 3
   },
   {
     id: '4fd25155-9cc2-4cd6-8852-3e0ca2d5e95d',
-    text: '不理解情绪化的人',
+    text: '不理解那些情绪化的人',
     keyed: 'minus',
     domain: 'O',
     facet: 3
   },
   {
     id: 'b68af20d-24f9-4c27-85cc-fe0858994888',
-    text: '不为别人浪费时间',
+    text: '不愿为他人花费时间',
     keyed: 'minus',
     domain: 'A',
     facet: 3
   },
   {
     id: '54423933-0ebb-44a7-bdd9-2a9b100c70f2',
-    text: '不守信',
+    text: '违背自己的承诺',
     keyed: 'minus',
     domain: 'C',
     facet: 3
   },
   {
     id: '7317848c-3e1b-422f-bb16-02efc504f677',
-    text: '没有社交困扰',
+    text: '不会被复杂的社交情境所困扰',
     keyed: 'minus',
     domain: 'N',
     facet: 4
   },
   {
     id: '7d93e1ca-46e8-4a30-9623-42a80c9b420c',
-    text: '喜欢放松',
+    text: '喜欢慢慢来',
     keyed: 'minus',
     domain: 'E',
     facet: 4
   },
   {
     id: 'a7f43928-8982-4ed5-8656-7a80346fe979',
-    text: '古板守旧',
+    text: '我喜欢传统的方式',
     keyed: 'minus',
     domain: 'O',
     facet: 4
@@ -764,35 +764,35 @@ const questions = [
   },
   {
     id: '3890bb43-2695-4b8d-b289-ee10d11cc884',
-    text: '花很少时间和精力在工作上',
+    text: '对工作或学业不怎么投入时间和精力',
     keyed: 'minus',
     domain: 'C',
     facet: 4
   },
   {
     id: '49a85680-53aa-4208-86b5-dccc7a6f8e37',
-    text: '能够控制住我的冲动',
+    text: '可以控制自己的欲望',
     keyed: 'minus',
     domain: 'N',
     facet: 5
   },
   {
     id: '10f90fa9-649c-4631-ac4c-3dd3f751597d',
-    text: '表现得疯狂',
+    text: '行为狂放不羁',
     keyed: 'plus',
     domain: 'E',
     facet: 5
   },
   {
     id: 'b86de003-c3c4-4cc8-9385-5ac8a0142c34',
-    text: '不喜欢讨论理论',
+    text: '不对理论性的讨论不感兴趣',
     keyed: 'minus',
     domain: 'O',
     facet: 5
   },
   {
     id: '80c1d149-7050-481a-9953-aefb441642e7',
-    text: '有良好的道德感',
+    text: '吹嘘自己的美德',
     keyed: 'minus',
     domain: 'A',
     facet: 5
@@ -806,35 +806,35 @@ const questions = [
   },
   {
     id: '88a3c2fe-3aa4-4f46-9322-da656332268a',
-    text: '在压力下保持冷静',
+    text: '在压力下能保持冷静',
     keyed: 'minus',
     domain: 'N',
     facet: 6
   },
   {
     id: 'e7b31bdc-5f6b-40ec-ba91-f5919b0f170e',
-    text: '对生活充满希望',
+    text: '对生活充满希望，看到生活中好的一面',
     keyed: 'plus',
     domain: 'E',
     facet: 6
   },
   {
     id: '580b08d1-3c94-46e9-9d07-d6d80c698127',
-    text: '相信我们应该眼里打击犯罪',
+    text: '相信我们应该严厉打击犯罪',
     keyed: 'minus',
     domain: 'O',
     facet: 6
   },
   {
     id: '48bee420-60c0-45cd-be43-3893dbc1969a',
-    text: '忽视那些需要帮助的人',
+    text: '尽量不去想那些需要帮助的人',
     keyed: 'minus',
     domain: 'A',
     facet: 6
   },
   {
     id: 'ea3327ea-3529-4be4-8e2d-2174731ae4d7',
-    text: '做事果断干脆',
+    text: '做事不经过思考',
     keyed: 'minus',
     domain: 'C',
     facet: 6
